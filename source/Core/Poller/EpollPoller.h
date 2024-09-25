769da3e70b345fad1297e23e6782f10c666877f6
chenjingyu <2458006366@qq.com>
2024-09-25 16:20:10 +0800

    Update：添加对epoll的注释；

--- source/Core/Poller/EpollPoller.h
+++ source/Core/Poller/EpollPoller.h
@@ -13,6 +13,12 @@
 #include "Base/Timestamp.h"
 
 NAMESPACE_BEGIN
+/**
+ * @brief Epoll封装：epoll解决调用IO函数时机
+ * epoll_create1：创建数据结构——红黑树和数据就绪队列
+ * epoll_ctl：注册事件，往红黑树中添加节点，并在增加操作时构建回调映射
+ * epoll_wait：将就绪队列中就绪事件拷贝到用户态
+ */
 class API EpollPoller : public Poller {
 public:
   using EventList = std::vector<epoll_event>;
